eaf0e6cd48108bbf88de73bca9b3d873cc1b0957
Aman Pal <aman.pal@tothenew.com>
2025-07-30 22:51:28 +0530

    first commit

---
+++ src/main/java/com/exercise/jpa3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.exercise.jpa3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Jpa3Application.class);
+	}
+
+}
